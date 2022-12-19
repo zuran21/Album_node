01060beef80d7c94d6be5a2a79ea0dab0d23f1f6
ashley zifrikc villanueva <72785236@certus.edu.pe>
2022-12-19 10:28:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+if (process.env.NODEENV !== 'production') {
+require('dotenv').config();
+}
+const app = require('./app');
+
+app.listen(app.get('port'), () =>{
+    console.log('servidor  on ', app.get('port'));
+    console.log('Environment :', process.env.NODEENV);
+});
